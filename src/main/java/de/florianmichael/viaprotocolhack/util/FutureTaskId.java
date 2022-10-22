5ddb5c87c8bfe0a5168588ce1dbef847d8c33634
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-10-23 01:00:15 +0200

    java 8

--- src/main/java/de/florianmichael/viaprotocolhack/util/FutureTaskId.java
+++ src/main/java/de/florianmichael/viaprotocolhack/util/FutureTaskId.java
@@ -4,7 +4,13 @@ import com.viaversion.viaversion.api.platform.PlatformTask;
 
 import java.util.concurrent.Future;
 
-public record FutureTaskId(Future<?> object) implements PlatformTask<Future<?>> {
+public class FutureTaskId implements PlatformTask<Future<?>> {
+
+    private final Future<?> object;
+
+    public FutureTaskId(Future<?> object) {
+        this.object = object;
+    }
 
     @Override
     public Future<?> getObject() {
