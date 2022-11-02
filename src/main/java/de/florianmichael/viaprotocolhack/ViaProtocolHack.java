16863ab0d1a2e755dd71289678ac76c806ecd5a5
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2022-11-02 16:37:58 +0100

    added support for older netty

--- src/main/java/de/florianmichael/viaprotocolhack/ViaProtocolHack.java
+++ src/main/java/de/florianmichael/viaprotocolhack/ViaProtocolHack.java
@@ -12,20 +12,20 @@ import de.florianmichael.viaprotocolhack.platform.viaversion.CustomViaInjector;
 import de.florianmichael.viaprotocolhack.util.JLoggerToLog4J;
 import de.florianmichael.viaprotocolhack.util.VersionList;
 import io.netty.channel.DefaultEventLoop;
+import io.netty.channel.DefaultEventLoopGroup;
 import io.netty.channel.EventLoop;
 import org.apache.logging.log4j.LogManager;
 
 import java.io.File;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.logging.Logger;
 
 public class ViaProtocolHack {
     private final static ViaProtocolHack instance = new ViaProtocolHack();
 
+    private final ThreadFactory factory = new ThreadFactoryBuilder().setDaemon(true).setNameFormat("ViaProtocolHack-%d").build();
     private final ExecutorService executorService = Executors.newFixedThreadPool(8, new ThreadFactoryBuilder().setDaemon(true).setNameFormat("ViaProtocolHack-%d").build());
-    private final EventLoop eventLoop = new DefaultEventLoop(executorService);
+    private final EventLoop eventLoop = new DefaultEventLoopGroup(1, (Executor) factory).next();
     private final Logger logger = new JLoggerToLog4J(LogManager.getLogger("ViaProtocolHack"));
 
     private INativeProvider provider;
