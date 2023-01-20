6ba23abf4fbaf38a9c32cafd93913709bd94db5d
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-01-20 21:23:04 +0100

    cleaned up native provider

--- src/main/java/de/florianmichael/vialoadingbase/NativeProvider.java
+++ src/main/java/de/florianmichael/vialoadingbase/NativeProvider.java
@@ -6,6 +6,7 @@ import com.viaversion.viaversion.api.protocol.version.VersionProvider;
 import com.viaversion.viaversion.bungee.providers.BungeeMovementTransmitter;
 import com.viaversion.viaversion.libs.gson.JsonObject;
 import com.viaversion.viaversion.protocols.protocol1_9to1_8.providers.MovementTransmitterProvider;
+import de.florianmichael.vialoadingbase.netty.NettyConstants;
 import de.florianmichael.vialoadingbase.provider.DefaultVersionProvider;
 import de.florianmichael.vialoadingbase.util.VersionListEnum;
 import io.netty.channel.EventLoop;
@@ -16,14 +17,21 @@ import java.util.concurrent.ThreadFactory;
 
 public interface NativeProvider {
 
+    // Default methods
+
+    File run();
     boolean isSinglePlayer();
     VersionListEnum nativeVersion();
-
-    String[] nettyOrder();
-    File run();
-    JsonObject createDump();
     EventLoop eventLoop(final ThreadFactory threadFactory, final ExecutorService executorService);
 
+    // Optional methods
+
+    default String[] nettyOrder() {
+        return NettyConstants.COMPRESSION_HANDLER_NAMES;
+    }
+    default JsonObject createDump() {
+        return new JsonObject();
+    }
     default void createProviders(final ViaProviders providers) {
         providers.use(MovementTransmitterProvider.class, new BungeeMovementTransmitter());
         providers.use(VersionProvider.class, new DefaultVersionProvider());
--- src/main/java/de/florianmichael/vialoadingbase/netty/NettyConstants.java
+++ src/main/java/de/florianmichael/vialoadingbase/netty/NettyConstants.java
@@ -2,6 +2,10 @@ package de.florianmichael.vialoadingbase.netty;
 
 public class NettyConstants {
 
+    // Via Version input constants
     public static final String HANDLER_DECODER_NAME = "via-decoder";
     public static final String HANDLER_ENCODER_NAME = "via-encoder";
+
+    // Minecraft constants
+    public static final String[] COMPRESSION_HANDLER_NAMES = new String[] { "decompress", "compress" };
 }
