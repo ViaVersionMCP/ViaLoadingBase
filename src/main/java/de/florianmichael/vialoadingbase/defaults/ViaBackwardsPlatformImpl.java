84509f9a56f441c07dd7843e88efcde7da8c615a
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-03-08 22:18:27 +0100

    changed year

--- src/main/java/de/florianmichael/vialoadingbase/defaults/ViaBackwardsPlatformImpl.java
+++ src/main/java/de/florianmichael/vialoadingbase/defaults/ViaBackwardsPlatformImpl.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of ViaLoadingBase - https://github.com/FlorianMichael/ViaLoadingBase
- * Copyright (C) 2023 FlorianMichael/EnZaXD and contributors
+ * Copyright (C) 2022-2023 FlorianMichael/EnZaXD and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
