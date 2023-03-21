a6bda59904911ab8c4360aac1f006b73d72e82b1
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-03-21 16:36:22 +0100

    removed imports

--- src/main/java/de/florianmichael/vialoadingbase/defaults/viaversion/VLBViaProviders.java
+++ src/main/java/de/florianmichael/vialoadingbase/defaults/viaversion/VLBViaProviders.java
@@ -18,12 +18,10 @@
 package de.florianmichael.vialoadingbase.defaults.viaversion;
 
 import com.viaversion.viaversion.api.Via;
-import com.viaversion.viaversion.api.connection.UserConnection;
 import com.viaversion.viaversion.api.platform.ViaPlatformLoader;
 import com.viaversion.viaversion.api.platform.providers.ViaProviders;
 import com.viaversion.viaversion.api.protocol.version.VersionProvider;
 import com.viaversion.viaversion.bungee.providers.BungeeMovementTransmitter;
-import com.viaversion.viaversion.protocols.base.BaseVersionProvider;
 import com.viaversion.viaversion.protocols.protocol1_9to1_8.providers.MovementTransmitterProvider;
 import de.florianmichael.vialoadingbase.ViaLoadingBase;
 import de.florianmichael.vialoadingbase.provider.VLBBaseVersionProvider;
